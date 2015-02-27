96252fca0fa4503e7f82829d8b9088697394777e
ahmedanwer <superahmedawa@gmail.com>
2015-02-27 22:18:22 +0200

    layout and fragment added

--- app/src/main/java/com/bixls/dollarprices/MainActivity.java
+++ app/src/main/java/com/bixls/dollarprices/MainActivity.java
@@ -51,23 +51,29 @@ public class MainActivity extends ActionBarActivity
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
         FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
+
+        switch (position) {
+            case 0:
+            fragmentManager.beginTransaction()
+                    .replace(R.id.container, PlaceholderFragment.newInstance(position))
+                    .commit();
+                break;
+            case 1:
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, Settings.newInstance(position))
+                        .commit();
+                break;
+           default:
+               fragmentManager.beginTransaction()
+                       .replace(R.id.container, PlaceholderFragment.newInstance(position))
+                       .commit();
+                break;
+        }
     }
 
     public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-        }
+        String[] headers=getResources().getStringArray(R.array.drawer_Array);
+                mTitle = headers[number];
     }
 
     public void restoreActionBar() {
@@ -110,16 +116,9 @@ public class MainActivity extends ActionBarActivity
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
+
         private static final String ARG_SECTION_NUMBER = "section_number";
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
         public static PlaceholderFragment newInstance(int sectionNumber) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
